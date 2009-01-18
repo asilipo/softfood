fb3d8e8658487f16f6e1d0fc18909172570ca71c
fpacilio@gmail.com <fpacilio@gmail.com@f3a17110-ca9e-11dd-bfe9-7982af55821d>
2009-01-18 08:56:27 +0000

    Add facade ArticoloMenuFacade

---
+++ src/trunk/SoftFood-ejb/src/java/it/softfood/facade/articolomenu/ArticoloMenuFacade.java
@@ -0,0 +1,15 @@
+package it.softfood.facade.articolomenu;
+
+import javax.ejb.Stateless;
+
+/**
+ * @author Maria Rosaria Paone
+ * @author Marco Grasso
+ * @author Francesco Pacilio
+ */
+
+@Stateless
+public class ArticoloMenuFacade implements ArticoloMenuFacadeRemote, ArticoloMenuFacadeLocal {
+    
+
+}
