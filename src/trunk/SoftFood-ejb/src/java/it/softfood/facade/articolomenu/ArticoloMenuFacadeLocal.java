703f2f9f92f2602b4689568eee073732a25a0c79
fpacilio <fpacilio@gmail.com>
2009-01-18 08:56:27 +0000

    Add facade ArticoloMenuFacade

---
+++ src/trunk/SoftFood-ejb/src/java/it/softfood/facade/articolomenu/ArticoloMenuFacadeLocal.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package it.softfood.facade.articolomenu;
+
+import javax.ejb.Local;
+
+/**
+ *
+ * @author nio
+ */
+@Local
+public interface ArticoloMenuFacadeLocal {
+    
+}
