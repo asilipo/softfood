fa8e34fa79033b21633d7e59797a3d5f9b8a5a21
fpacilio@gmail.com <fpacilio@gmail.com@f3a17110-ca9e-11dd-bfe9-7982af55821d>
2009-01-19 12:00:39 +0000

    Management session exception

--- src/trunk/SoftFood-ejb/src/java/it/softfood/entity/Bevanda.java
+++ src/trunk/SoftFood-ejb/src/java/it/softfood/entity/Bevanda.java
@@ -2,10 +2,13 @@ package it.softfood.entity;
 
 import java.io.Serializable;
 
+import javax.persistence.Column;
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
+import javax.persistence.MappedSuperclass;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
+import javax.persistence.Table;
 
 /**
  * @author Maria Rosaria Paone
@@ -16,12 +19,23 @@ import javax.persistence.NamedQuery;
 @Entity
 @DiscriminatorValue(value = "Bevanda")
 @NamedQueries({
-        @NamedQuery(name = "Bevanda.selezionaBevande", query = "SELECT b FROM Bevande b")
+        @NamedQuery(name = "Bevanda.selezionaBevande", query = "SELECT b FROM Bevanda b")
 })
 public class Bevanda extends Articolo implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
+    @Column(name = "capacita", nullable = true)
+	private Float capacita;
+
+    public Float getCapacita() {
+        return capacita;
+    }
+
+    public void setCapacita(Float capacita) {
+        this.capacita = capacita;
+    }
+   
 	@Override
 	public boolean equals(Object obj) {
 		return super.equals(obj);
