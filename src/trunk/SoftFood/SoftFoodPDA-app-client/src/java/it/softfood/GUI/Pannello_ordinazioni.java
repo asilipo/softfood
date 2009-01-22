a0b4cbe3dce10a3c5195f5c55a5eedb57ca5b9b8
fpacilio@gmail.com <fpacilio@gmail.com@f3a17110-ca9e-11dd-bfe9-7982af55821d>
2009-01-22 17:46:31 +0000

    Management session exception

--- src/trunk/SoftFood/SoftFoodPDA-app-client/src/java/it/softfood/GUI/Pannello_ordinazioni.java
+++ src/trunk/SoftFood/SoftFoodPDA-app-client/src/java/it/softfood/GUI/Pannello_ordinazioni.java
@@ -332,7 +332,6 @@ private void visuallizzaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-F
     int row = tabella_pietanza.getSelectedRow();
     id_antipasto.setColumnVisible(id, true);
     long id_long = ((Long) tabella_pietanza.getValueAt(row, 0)).longValue();
-    System.out.println("ID pietanza " + id_long);
     id_antipasto.setColumnVisible(id, false);
     this.setVisible(false);
     it.softfood.GUI.Pietanza pietanza = new it.softfood.GUI.Pietanza(frame, tavolo, id_long, tipo);
