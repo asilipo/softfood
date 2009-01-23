9d850c091d2b04764cd0cb7625dcc3789dc21d81
fpacilio <fpacilio@gmail.com>
2009-01-23 17:10:18 +0000

    Management session exception

--- src/trunk/SoftFood/SoftFoodPDA-app-client/src/java/it/softfood/GUI/ToolTipCellRender.java
+++ src/trunk/SoftFood/SoftFoodPDA-app-client/src/java/it/softfood/GUI/ToolTipCellRender.java
@@ -2,9 +2,13 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package it.softfood.GUI;
 
+import it.softfood.entity.Variante;
+import java.awt.Color;
+import java.awt.Component;
+import java.util.ArrayList;
+import javax.swing.JTable;
 import javax.swing.table.DefaultTableCellRenderer;
 
 /**
@@ -13,16 +17,39 @@ import javax.swing.table.DefaultTableCellRenderer;
  */
 public class ToolTipCellRender extends DefaultTableCellRenderer {
 
-    public ToolTipCellRender(){
-        super();
-    }
+    private ArrayList<Variante> varianti;
     
+    public ToolTipCellRender(ArrayList<Variante> varianti) {
+        super();
+        this.varianti = varianti;
+    }
+
     public void setValue(String aValue) {
-    String result = aValue;
-    if ( (aValue != null)) {
-      StringBuilder tooltip = new StringBuilder(result);
-      setToolTipText( tooltip.toString() );
-    } 
-    super.setValue(result);
-  }   
+        String result = aValue;
+        if ((aValue != null)) {
+            StringBuilder tooltip = new StringBuilder(result);
+            setToolTipText(tooltip.toString());
+        }
+        super.setValue(result);
+    }
+
+    @Override
+    public Component getTableCellRendererComponent(JTable table,
+            Object obj, boolean isSelected, boolean hasFocus, int row, int column) {
+        Component cell = super.getTableCellRendererComponent(
+                table, obj, isSelected, hasFocus, row, column);
+        setToolTipText("Ciao  " + row);
+        if (isSelected) {
+            cell.setBackground(Color.green);
+            
+        } else {
+            if (row % 2 == 0) {
+                cell.setBackground(Color.cyan);
+            } else {
+                cell.setBackground(Color.lightGray);
+            }
+        }
+        return cell;
+    }
+
 }
